533d1224a78aed42b680b6a41d3261bb2cc54141
Beernaerts Johnny <beernaerts.johnny@gmail.com>
2016-06-21 04:26:13 +0200

    Initial upload

---
+++ SS13Try/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMover : MonoBehaviour 
+{
+    #region constants
+    #endregion
+
+    #region variables
+    private Rigidbody m_Rb;
+    public float m_Speed = 10.0f;
+    public float m_JumpSpeed = 15.0f;
+    #endregion
+
+    #region fields
+    #endregion
+
+    #region functions
+    void Start()
+    {
+        m_Rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        CatchInput();
+        //ApplyGravity();
+    }
+
+    void CatchInput()
+    {
+        Vector3 _Direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        _Direction *= m_Speed;
+        if ( Input.GetKeyDown(KeyCode.Space) )
+        {
+            _Direction.y = m_JumpSpeed;
+        }
+        _Direction += Physics.gravity;
+        //_Direction *= Time.deltaTime;
+        m_Rb.AddForce(_Direction, ForceMode.Acceleration);
+    }
+
+    void ApplyGravity()
+    {
+        m_Rb.AddForce(Physics.gravity * Time.deltaTime);
+    }
+	#endregion
+	
+	#region events
+	#endregion
+}
---
+++ SS13Try/Assets/Scripts/TestManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TestManager : MonoBehaviour 
+{
+    #region constants
+    #endregion
+
+    #region variables
+    public Text m_Text;
+    public Body m_Body;
+	#endregion
+	
+	#region fields
+	#endregion
+	
+	#region functions
+    public void Start()
+    {
+        m_Body.BodyParts[0].DamageReceived += DebugDamage;
+    }
+
+    public void Update()
+    {
+        m_Text.text = "";
+        foreach( BodyPart bp in m_Body.BodyParts )
+        {
+            m_Text.text += bp.Name + " : " + bp.HP + " / " + bp.MaxHP + "\n";
+        }
+    }
+
+    private void DebugDamage( object sender, BodyPart.DamageReceivedEventArgs e )
+    {
+        string _text = (sender as BodyPart).Name + " took " + e.damage + ".";
+        if (e.damageKilled)
+            _text += " It destroyed it.";
+        Debug.Log(_text);
+    }
+	#endregion
+	
+	#region events
+	#endregion
+}
